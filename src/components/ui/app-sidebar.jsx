0c2366fdd67bbd37b0c2c8bbafe3ebc83b2473bd
MajidKh230 <khanmajid230@gmail.com>
2024-11-04 13:08:44 +0500

    add SignOut

--- src/components/ui/app-sidebar.jsx
+++ src/components/ui/app-sidebar.jsx
@@ -1,69 +1,99 @@
+"use client";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-  } from "@/components/ui/sidebar"
-   
-import { Calendar, DollarSign, Home,  ListOrderedIcon,  LogOut,  LucideShoppingBag, Search, Settings, User2 } from "lucide-react"
-import Link from "next/link"
- 
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import logoutAlert from "../logoutAlert";
+
+import {
+  Calendar,
+  DollarSign,
+  Home,
+  ListOrderedIcon,
+  LogOut,
+  LogOutIcon,
+  LucideShoppingBag,
+  Search,
+  Settings,
+  User2,
+} from "lucide-react";
+import Link from "next/link";
+import { toast } from "react-toastify";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
+
 export function AppSidebar() {
-    const items = [
-        {
-          title: "Dashboard",
-          url: "/admin",
-          icon: Home,
-        },
-        {
-          title: "Products",
-          url: "/products",
-          icon: LucideShoppingBag,
-        },
-        {
-          title: "Payments",
-          url: "/payments",
-          icon: Calendar,
-        },
-        {
-          title: "Orders",
-          url: "/orders",
-          icon: ListOrderedIcon,
-        },
-        {
-          title: "Enquiry",
-          url: "/enquiry",
-          icon: Search,
-        },
-        {
-          title: "Marketing",
-          url: "/marketing",
-          icon: DollarSign,
-        },
-        {
-          title: "Setting",
-          url: "/setting",
-          icon: Settings,
-        },
-        {
-          title: "User",
-          url: "/user",
-          icon: User2,
-        },
-        {
-          title: "Logout",
-          url: "/logout",
-          icon: LogOut,
-        },
-      ]
+  const HandleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("you have sucessfully logout!");
+        Router.push("/");
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        console.log(error.message);
+
+        // An  happened.
+      });
+  };
+
+  const items = [
+    {
+      title: "Dashboard",
+      url: "/admin",
+      icon: Home,
+    },
+    {
+      title: "Products",
+      url: "/products",
+      icon: LucideShoppingBag,
+    },
+    {
+      title: "Payments",
+      url: "/payments",
+      icon: Calendar,
+    },
+    {
+      title: "Orders",
+      url: "/orders",
+      icon: ListOrderedIcon,
+    },
+    {
+      title: "Enquiry",
+      url: "/enquiry",
+      icon: Search,
+    },
+    {
+      title: "Marketing",
+      url: "/marketing",
+      icon: DollarSign,
+    },
+    {
+      title: "Setting",
+      url: "/setting",
+      icon: Settings,
+    },
+    {
+      title: "User",
+      url: "/user",
+      icon: User2,
+    },
+    // {
+    //   title: "Logout",
+    //   url: "/logout",
+    //   icon: LogOut,
+    // },
+  ];
   return (
     <Sidebar>
-      <SidebarContent >
-      <SidebarGroup>
+      <SidebarContent>
+        <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
@@ -78,9 +108,13 @@ export function AppSidebar() {
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
+            <SidebarMenuButton onClick={HandleLogout}>
+              <LogOutIcon />
+              <span>LogOut</span>
+            </SidebarMenuButton>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
-  )
+  );
 }
